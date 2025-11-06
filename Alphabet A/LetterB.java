8a3c30b9fa81997f4ac6e050cd515c48310b45ea
TechbyWebCoder <techbywedcoder@gmail.com>
2025-11-06 19:05:29 +0530

    Add LetterB.java to print letter pattern
    Implement a program to print the letter 'B' pattern using asterisks.

---
+++ Alphabet A/LetterB.java
@@ -0,0 +1,27 @@
+// Alphabet Pattern Program = B
+
+/* 
+
+****** 
+*     *
+****** 
+*     *
+******
+
+*/
+
+public class LetterB {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1; j<=7;j++){
+                if(j==1 || (i==1 && j<7) || (i==3 && j<7) || (i==5 && j<7) || (j==7 && i !=1 && i!=3 && i!=5)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
