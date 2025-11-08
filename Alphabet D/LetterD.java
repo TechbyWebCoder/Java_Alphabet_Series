d9d0da678e24eef3083cf3c59bc7cca602caed80
TechbyWebCoder <techbywedcoder@gmail.com>
2025-11-08 18:52:34 +0530

    Add LetterD.java for printing letter D pattern

---
+++ Alphabet D/LetterD.java
@@ -0,0 +1,27 @@
+// Alphabet Pattern Program = D
+
+/* 
+
+****** 
+*     *
+*     *
+*     *
+******
+
+*/
+
+public class LetterD {
+    public static void main(String[] args) {
+        for(int i=1; i<=5;i++){
+            for(int j=1;j<=7;j++){
+                if(j==1 || (j==7 && i>1 && i<5) || ((i==1 || i==5) && j<7)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
