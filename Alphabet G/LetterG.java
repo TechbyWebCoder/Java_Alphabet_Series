017a0d0a746b7364f4a84b68878636a8966f3ecc
TechbyWebCoder <techbywedcoder@gmail.com>
2025-11-11 18:13:54 +0530

    Add LetterG.java for alphabet pattern G
    Implement a program to print the letter 'G' pattern.

---
+++ Alphabet G/LetterG.java
@@ -0,0 +1,27 @@
+// Alphabet Pattern Program = G
+
+/* 
+
+*******
+*      
+*   ***
+*     *
+*******
+
+*/
+
+public class LetterG {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1; j<=7; j++){
+                if(j==1 || (i==1 && j>1) ||(i==5 && j<7) || (j==7 && i>3) || (i==3 && j>4)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
