4a3a10b218ffbf46728c44343d88e7654a509671
TechbyWebCoder <techbywedcoder@gmail.com>
2025-11-15 18:22:07 +0530

    Create LetterK.java to display letter 'K' pattern
    Implemented a program to print the letter 'K' pattern.

---
+++ Alphabet K/LetterK.java
@@ -0,0 +1,27 @@
+// Alphabet Pattern Program = K
+
+/*
+
+*   *
+*  * 
+* *  
+*  *
+*   *
+
+*/
+
+public class LetterK {
+    public static void main(String[] args) {
+        for(int i=1; i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(j==1 || j==6-i && i<3 || j==i && i>=3){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
