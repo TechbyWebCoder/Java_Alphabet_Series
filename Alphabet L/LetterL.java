6e5e4826a12eb642149f0832347356769a343e16
TechbyWebCoder <techbywedcoder@gmail.com>
2025-11-16 18:13:42 +0530

    Add LetterL.java to print 'L' pattern
    This program prints the letter 'L' pattern using asterisks.

---
+++ Alphabet L/LetterL.java
@@ -0,0 +1,27 @@
+// Alphabet Pattern Program = L
+
+/* 
+
+*    
+*    
+*    
+*
+*****
+
+*/
+
+public class LetterL {
+    public static void main(String[] args) {
+        for(int i=1; i<=5;i++){
+            for(int j=1; j<=5;j++){
+                if(j==1 || i==5){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.err.println();
+        }
+    }
+}
