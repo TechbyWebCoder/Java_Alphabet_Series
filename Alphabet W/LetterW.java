45a033ba2d56bb2aa7b670c50d1e5dfbad02be15
TechbyWebCoder <techbywedcoder@gmail.com>
2025-11-27 18:19:03 +0530

    Add LetterW class to print letter pattern
    This program prints the letter 'W' using asterisks in a specific pattern.

---
+++ Alphabet W/LetterW.java
@@ -0,0 +1,27 @@
+// Alphabet Pattern Program = W
+
+/* 
+
+*       *
+*       *
+*   *   *
+*  * *  *
+* *   * *
+
+*/
+
+public class LetterW {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=5;i++){
+            for(int j=1; j<=5*2-1;j++){
+                if(j==1 || j==5*2-1 || (i==3 && j==n) || (i==4 && (j==n-1 || j==n+2)) || (i==5 && (j==n-2 || j==n+2))){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
