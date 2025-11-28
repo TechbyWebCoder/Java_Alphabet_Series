7639a7be8cc50c9172cb1917b0fc3e7a2d4aef7c
TechbyWebCoder <techbywedcoder@gmail.com>
2025-11-28 18:12:26 +0530

    Add LetterX.java to print 'X' pattern
    Implement a program to print the letter 'X' pattern.

---
+++ Alphabet X/LetterX.java
@@ -0,0 +1,26 @@
+// Alphabet Pattern Program = X
+
+/* 
+
+*   *
+ * * 
+  *  
+ * *
+*   *
+
+*/
+
+public class LetterX {
+    public static void main(String[] args) {
+        for(int i=1; i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i==j || j+i==6){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
